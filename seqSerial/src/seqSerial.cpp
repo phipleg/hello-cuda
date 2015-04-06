7751789765f1f7950061799a72401fe8970ff053
phipleg <phipleg@gmx.de>
2015-04-06 14:47:41 +0200

    Initial commit

---
+++ seqSerial/src/seqSerial.cpp
@@ -0,0 +1,25 @@
+//seqSerial.cpp
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    const int N=5000;
+    vector<int> a(N);
+    for(int i=0; i<N; i++) a[i]=i;
+
+    int sumA=0;
+    for (int i=0; i<N; i++) sumA += a[i];
+
+    int sumCheck=0;
+    for (int i=0; i<N; i++) sumCheck += i;
+
+    if (sumA == sumCheck) cout << "Test Succeeded!" << endl;
+    else {
+        cerr << "Test FAILED!";
+        return(1);
+    }
+    return(0);
+}
